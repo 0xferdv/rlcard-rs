e011746b9e1eb87b3a4393fece4541bd2913ad1b
JoyfulRusty <qdsjprs@gmail.com>
2025-06-22 17:06:36 +0800

    add burn crate

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -1,5 +1,20 @@
+#![recursion_limit = "256"]
+
+
+use burn::tensor::Tensor;
+use burn::backend::Wgpu;
+
+// Type alias for the backend to use.
+type Backend = Wgpu;
+
 fn main() {
-    let a = 10;
-    let b = 20;
-    println!("{} + {} = {}", a, b, a + b);
+    let device = Default::default();
+    // Creation of two tensors, the first with explicit values and the second one with ones, with the same shape as the first
+    let tensor_1 = Tensor::<Backend, 2>::from_data([[2., 3.], [4., 5.]], &device);
+    let tensor_2 = Tensor::<Backend, 2>::ones_like(&tensor_1);
+
+    // Print the element-wise addition (done with the WGPU backend) of the two tensors.
+    println!("{}", tensor_1 + tensor_2);
 }
+
+
